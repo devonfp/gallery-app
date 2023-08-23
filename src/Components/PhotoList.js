bc89e7dd175dc0231e95402cbde3052b5496cbd0
Devon Poston <devonposton1@gmail.com>
2023-08-23 15:32:50 -0400

    Fixed URL error

--- src/Components/PhotoList.js
+++ src/Components/PhotoList.js
@@ -8,11 +8,11 @@ const PhotoList = props => {
   //console.log(results.length);
   if (results.photos.photo.length > 0) {
     photos = results.photos.photo.map((photo) => {
-     const url = `https://live.staticflickr.com/${photo.server_id}/${photo.id}_${photo.secret}_b.jpg`;  
+     const url = `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;  
     console.log(url);
     return(
       <ul className="photo-wrap">
-     <img src={url} alt={photo.title} key={photo.id}/> 
+     <img src={url} alt={photo.title} key={photo.id} width={'100%'} height={'auto'}/> 
       </ul> 
     );
   }); 
