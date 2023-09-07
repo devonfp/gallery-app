968a47d4f9159b78cd12bbec89b5cbbfad7a7829
Devon Poston <devonposton1@gmail.com>
2023-09-07 12:21:55 -0400

    CSS updates

--- src/Components/PhotoList.js
+++ src/Components/PhotoList.js
@@ -7,14 +7,15 @@ If there are photos in the data prop, they are displayed.
 If there are no photos in the data prop, a "NotFound" component is displayed.*/
 const PhotoList = props => { 
   const results = props.data;
+  const size = 'm'
   let photos = [];
 
   if (results.photos?.photo.length > 0) {
     photos = results.photos.photo.map((photo) => {
-     const url = `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;  
+     const url = `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_${size}.jpg`;  
     console.log(url);
     return(
-  <ul className="photo-wrap" key={photo.id}>
+  <ul className="photo-container" key={photo.id}>
       <li key={photo.id}>
      <img src={url} alt={photo.title}/> 
      </li>
